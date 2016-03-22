853e47646009e216674871339c71610c3478f384
Lieke <liekevdpoel@gmail.com>
2016-03-22 17:05:50 +0100

    first questions for risk tolerance

--- app/src/users/UserService.js
+++ app/src/users/UserService.js
@@ -19,9 +19,53 @@
         avatar: 'svg-2',
         content: "I invest in people's happiness.",
         questions: [
-          {no: 1, text: "aha", score: 3},
-          {no: 2, text: "baha", score: 5},
-          {no: 3, text: "muha", score: 10}
+          {no: 1, text: "How would your best friend describe you as a risk taker?", answers: [
+            {answer: "A real risk avoider", score: 1},
+            {answer: "Willing to take risks after completing adequate research", score: 2},
+            {answer: "Cautious", score: 3},
+            {answer: "A real gambler", score: 4}
+          ]},
+          {no: 2, text: "You are on a TV gameshow and you can choose one of the following. Which would you choose?", answers: [
+            {answer: "$1000 in cash", score: 1},
+            {answer: "A 50 percent chance at winning $5000", score: 2},
+            {answer: "A 25 percent chance at winning $10.000", score: 3},
+            {answer: "A 5 percent chance at winning $100.000", score: 4}
+          ]},
+          {no: 3, text: "You have just finished saving saving for a once-in-a-lifetime vacation, but you loose your job. What would you do?", answers: [
+            {answer: "Cancel the vacation", score: 1},
+            {answer: "Take a much more modest vacation", score: 2},
+            {answer: "Go as scheduled. Reasoning that you need to to prepare for a job search", score: 3},
+            {answer: "Extend your vacation because this might be the last time you travel first class", score: 4}
+          ]},
+          {no: 4, text: "If you unexpectedly receive $20.000 to invest, what would you do?", answers: [
+            {answer: "", score: 1},
+            {answer: "", score: 2},
+            {answer: "", score: 3}
+          ]},
+          {no: 5, text: "?", answers: [
+            {answer: "", score: 1},
+            {answer: "", score: 2},
+            {answer: "", score: 3},
+            {answer: "", score: 4}
+          ]},
+          {no: 1, text: "?", answers: [
+            {answer: "", score: 1},
+            {answer: "", score: 2},
+            {answer: "", score: 3},
+            {answer: "", score: 4}
+          ]},
+          {no: 1, text: "?", answers: [
+            {answer: "", score: 1},
+            {answer: "", score: 2},
+            {answer: "", score: 3},
+            {answer: "", score: 4}
+          ]},
+          {no: 1, text: "?", answers: [
+            {answer: "", score: 1},
+            {answer: "", score: 2},
+            {answer: "", score: 3},
+            {answer: "", score: 4}
+          ]}
         ]
       },
       {
